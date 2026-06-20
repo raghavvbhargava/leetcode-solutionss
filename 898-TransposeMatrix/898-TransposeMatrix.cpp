589b30540259375cfcc77f42e46546965c43057c
RB <142412511+raghavvbhargava@users.noreply.github.com>
2026-06-20 17:07:35 +0530

    Create 898-TransposeMatrix

---
+++ 898-TransposeMatrix/898-TransposeMatrix.cpp
@@ -0,0 +1,18 @@
+// Last updated: 6/20/2026, 5:07:35 PM
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        vector<vector<int>> ans(col, vector<int>(row));
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+
+        return ans;
+    }
+};
