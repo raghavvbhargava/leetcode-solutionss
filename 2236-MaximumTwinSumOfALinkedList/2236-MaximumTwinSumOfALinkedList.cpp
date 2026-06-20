793b4c799a6ccc70ab10c39a14ea0c04e0c089dc
RB <142412511+raghavvbhargava@users.noreply.github.com>
2026-06-20 17:07:22 +0530

    Create 2236-MaximumTwinSumOfALinkedList

---
+++ 2236-MaximumTwinSumOfALinkedList/2236-MaximumTwinSumOfALinkedList.cpp
@@ -0,0 +1,46 @@
+// Last updated: 6/20/2026, 5:07:23 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        ListNode* prev = nullptr;
+        while (slow) {
+            ListNode* nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        // Calculate maximum twin sum
+        int maxSum = 0;
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            maxSum = max(maxSum, first->val + second->val);
+            first = first->next;
+            second = second->next;
+        }
+
+        return maxSum;
+    }
+};
