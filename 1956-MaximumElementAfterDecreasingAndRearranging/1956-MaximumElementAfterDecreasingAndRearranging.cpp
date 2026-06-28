207815687efe25d879bd3400f5a64c3ebebf9ebe
RB <142412511+raghavvbhargava@users.noreply.github.com>
2026-06-28 12:22:05 +0530

    Create 1956-MaximumElementAfterDecreasingAndRearranging

---
+++ 1956-MaximumElementAfterDecreasingAndRearranging/1956-MaximumElementAfterDecreasingAndRearranging.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/28/2026, 12:22:06 PM
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        arr[0] = 1;
+        for (int i = 0; i < arr.size() - 1; i++) {
+            if (arr[i + 1] > arr[i] + 1) {
+                arr[i + 1] = arr[i] + 1;
+            }
+        }
+
+        int k = *max_element(arr.begin(), arr.end());
+        return k;
+    }
+};
